bc038c2c9311ad7e2eb400ca28faeb59daf0dd4a
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-17 16:49:08 -0400

    added a string manipulator function.

--- strings.py
+++ strings.py
@@ -1,3 +1,6 @@
+from more_itertools import strip
+
+
 def palindr():
     words = ["kayak", "wow", "defied", "military", "poem", "racecar"]
     # words = "kayak"
@@ -10,3 +13,25 @@ def palindr():
         print(palindrome)
 
 palindr()
+
+def w_ratio():
+    words = "Anathema"
+    for word in range(1,len(words)-1, 2):
+        print(f"The index is at {word,word+1}:{words[word:word+2]}")
+    # The result with the {word,word+1} is that the index is stored 
+    # in a tuple.
+w_ratio()
+
+def str_case():
+    words = ["I", "am", "going", "to", "the", "supermarket", "!"]
+    quote = " "
+    for word in range(0,len(words)):
+        if len(words) > 0:
+            quote += words[word] + " "
+        else:
+            quote += words[word]
+    return quote.strip()
+#Tech debt: figure out how to close out "!" into the last word."
+
+print(str_case())
+                     
